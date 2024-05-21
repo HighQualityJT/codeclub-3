c3645f289fdd48fba3740f3d62099edf93166823
HighQualityJT <eagle60jt@yahoo.com>
2024-05-21 13:25:23 -0400

    New Commit For Testing v2

--- code-club-3/src/index.js
+++ code-club-3/src/index.js
@@ -1,15 +1,25 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request) {
+	  /**
+	   * A map of the URLs to redirect to
+	   * @param {Object} countryMap
+	   */
+	  const countryMap = {
+		US: "https://example.com/us",
+		EU: "https://example.com/eu",
+	  };
+  
+	  // Use the cf object to obtain the country of the request
+	  // more on the cf object: https://developers.cloudflare.com/workers/runtime-apis/request#incomingrequestcfproperties
+	  const country = request.cf.country;
+  
+	  if (country != null && country in countryMap) {
+		const url = countryMap[country];
+		// Remove this logging statement from your final output.
+		console.log(`Based on ${country}-based request, your user would go to ${url}.` )
+		return Response.redirect(url);
+	  } else {
+		return fetch("https://example.com", request);
+	  }
 	},
-};
+  };
